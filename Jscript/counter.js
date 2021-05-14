37a143c1116ddf8e432cfc12df1549e755007386
Gianbho <gianluca.trm@gmail.com>
2021-05-14 14:22:27 +0200

    update
    update

--- Jscript/counter.js
+++ Jscript/counter.js
@@ -11,54 +11,46 @@ divCount.appendChild(countScreen);
 
 //--------------------------------------------
 
-function newButton1() {
+function newButtons() {
 let divCount = document.getElementById("counter");
-let btn = document.createElement("button");
+let btn1 = document.createElement("button");
 
-btn.innerHTML = "+";
-btn.type = "button";
-btn.name = "button";
-btn.className = "counter-button";
-btn.onclick = function() {
+btn1.innerHTML = "+";
+btn1.type = "button";
+btn1.name = "button";
+btn1.className = "counter-button";
+btn1.onclick = function() {
   let countScreen = document.getElementsByClassName("count");
   let number = countScreen[0].innerHTML = ++numValue;
 };
 
-divCount.appendChild(btn);
-}
+divCount.appendChild(btn1);
 
-//---------------------------------------------
+let btn2 = document.createElement("button");
 
-function newButton2() {
-let divCount = document.getElementById("counter");
-let btn = document.createElement("button");
-
-btn.innerHTML = "-";
-btn.type = "button";
-btn.name = "button";
-btn.className = "counter-button";
-btn.onclick = function() {
+btn2.innerHTML = "-";
+btn2.type = "button";
+btn2.name = "button";
+btn2.className = "counter-button";
+btn2.onclick = function() {
   let countScreen = document.getElementsByClassName("count");
   let number = countScreen[0].innerHTML = --numValue;
 };
 
-divCount.appendChild(btn);
-}
+divCount.appendChild(btn2);
 
-//---------------------------------------
 
-function newButton3() {
-let divCount = document.getElementById("counter");
-let btn = document.createElement("button");
+let btn3 = document.createElement("button");
 
-btn.innerHTML = "reset";
-btn.type = "button";
-btn.name = "button";
-btn.className = "counter-button button-reset";
-btn.onclick = function() {
+btn3.innerHTML = "reset";
+btn3.type = "button";
+btn3.name = "button";
+btn3.className = "counter-button button-reset";
+btn3.onclick = function() {
   let countScreen = document.getElementsByClassName("count");
   let number = countScreen[0].innerHTML = (numValue = 0);
 };
 
-divCount.appendChild(btn);
+divCount.appendChild(btn3);
 }
+//---------------------------------------------
